4166613a852b9fddfce1914441aa4385555459c0
HpyHacking <crazyflapjack@gmail.com>
2012-08-18 06:04:35 +0800

    change OTP config

--- src/webtekcos_app.erl
+++ src/webtekcos_app.erl
@@ -3,14 +3,11 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/0, start/2, stop/1]).
+-export([start/2, stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
-
-start() ->
-  application:start(webtekcos).
 
 start(_StartType, _StartArgs) ->
   webtekcos_sup:start_link().
